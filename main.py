a4d7ba2340aa0919ea1b14857df4dd07745b0f47
not-wish <visheshagarwaltsk@gmail.com>
2025-08-30 21:05:53 +0530

    Made changes to main.py with calling of functions from printing and calculate

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import printing
+import printing as p
+import calculate as c 
 print("Hello to everyone!")
 
 name = input("Enter your name: ")
-printing.printName(name)
+p.printName(name)
+p.Greet()
+
+c.add(10,5)
+c.sub(5, 10)
+c.mul(48, 6)
+c.div(9,3)
